f840c737a1a0ea820abae79c6c9556edefef992e
Hebes <hebes@mail.ru>
2016-02-19 14:15:45 +0300

    hebes master

--- src/clan/tremere/Main.java
+++ src/clan/tremere/Main.java
@@ -1,10 +1,14 @@
 package clan.tremere;
 
+import javax.swing.*;
+
 /**
  * Created by Hebes on 19.02.2016.
  */
 public class Main {
     public static void main(String[] args) {
-
+        JFrame frame=new JFrame();
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 }
